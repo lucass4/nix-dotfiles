a6ecba8e341427668fdc659fda5714ade1cd545f
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2026-01-08 07:45:33 -0800

    add catpuchino yazi theme

--- modules/home-manager/modules/yazi.nix
+++ modules/home-manager/modules/yazi.nix
@@ -1,6 +1,25 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  catppuccinMacchiato = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "flavors";
+    rev = "4a1802a5add0f867b08d5890780c10dd1f051c36";
+    sha256 = "1k0ricziqap8l3l3f9wbzybdgmmd2472f7kvz7al5grxp3n7vca6";
+  };
+in
+{
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+  };
+
+  xdg.configFile."yazi/theme.toml".text = ''
+    [flavor]
+    dark = "catppuccin-macchiato"
+  '';
+
+  xdg.configFile."yazi/flavors/catppuccin-macchiato.yazi" = {
+    source = "${catppuccinMacchiato}/catppuccin-macchiato.yazi";
+    recursive = true;
   };
 }
